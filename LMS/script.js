10265492fa5a419f514a223e68a70f06aa8a588e
DavidStarshaw <david@waora.school.nz>
2016-09-04 12:33:45 +1200

    Alert test 2

--- LMS/script.js
+++ LMS/script.js
@@ -1,11 +1,1 @@
-if (auth2.isSignedIn.get()) {
-  var profile = auth2.currentUser.get().getBasicProfile();
-  console.log('ID: ' + profile.getId());
-  console.log('Full Name: ' + profile.getName());
-  console.log('Given Name: ' + profile.getGivenName());
-  console.log('Family Name: ' + profile.getFamilyName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail());
-}
-
-alert("Hi there, and welcome.")
+alert("Hi there, and welcome from th JS.")
