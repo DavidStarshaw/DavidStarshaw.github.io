63359c15023d7cc62b339d6330bc414007f15700
DavidStarshaw <david@waora.school.nz>
2016-09-04 18:23:12 +1200

    login2 test 1

---
+++ LMS/login2.js
@@ -0,0 +1,13 @@
+document.getElementById("img").onclick = function() {
+	alert("Hello world");
+};
+
+alert("Hello world");
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
