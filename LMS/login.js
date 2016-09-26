73bd338623219abb4c99af08de90b945ac1d3f86
DavidStarshaw <david@waora.school.nz>
2016-09-26 18:04:33 +1300

    Update

--- LMS/login.js
+++ LMS/login.js
@@ -1,18 +1,10 @@
-document.getElementById("img").onclick = function() {
-	var profile = googleUser.getBasicProfile();
-	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-	console.log('Name: ' + profile.getName());
-	console.log('Image URL: ' + profile.getImageUrl());
-	console.log('Email: ' + profile.getEmail());
-};
-
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail());
-  //window.location.href = "home.html";
+  window.location.href = "home.html";
 }
 
 function signOut() {
@@ -21,3 +13,11 @@ function signOut() {
 	  console.log('User signed out.');
 	});
 }
+
+document.getElementById("img").onclick = function() {
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail());
+};
